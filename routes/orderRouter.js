732e72a1fc2d55966a2b65f4c9147327e31fbb5e
Full Stack Developer <104979478+PhoenixDev1223@users.noreply.github.com>
2023-01-06 20:43:09 +0800

    NodeMailer Integration

--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
-const Stripe = require("stripe")
+const Stripe = require("stripe");
+const nodemailer = require("nodemailer");
 const auth = require("../middleware/auth");
-const Order = require("../models/orderModel");
+const User = require("../models/userModel");
 
 const stripe = new Stripe(
   `${process.env.STRIPE_SK}`,
@@ -40,8 +41,47 @@ router.post("/", auth, async (req, res) => {
     })
 
     await newOrder.save()
+
+    const user = await User.findById(userId)
+    let html = `<html><head><style>table, tr, th, td {font-family: Arial, sans-serif; border:2px solid #000000 !important; border-collapse: collapse;}</style></head><body><table><thead><tr><th>No</th><th>Product</th><th>Price</th><th>Count</th><th></th></tr></thead><tbody>`
+    let totalPrice = 0
+
+    order.orders.forEach((item, index) => {
+      html += `<tr>
+        <td>${index + 1}</td>
+        <td>${item.title}</td>
+        <td>$${item.price}</td>
+        <td>${item.count}</td>
+        <td>$${item.count * item.price}</td>
+      </tr>`
+      totalPrice += item.count * item.price
+    });
+
+    html += `<tr><td></td><td></td><td></td><td></td><td>$${totalPrice}</td></tr></tbody></table></body></html>`
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      service: 'Gmail',
+      auth: {
+        user: "food.hero.mevada@gmail.com", // generated ethereal user
+        pass: "fouklslweulennnl", // generated ethereal password
+      },
+    });
+
+    // send mail with defined transport object
+    await transporter.sendMail({
+      from: 'food.hero.mevada@gmail.com', // sender address
+      to: user.email, // list of receivers
+      subject: "Order Confirmation", // Subject line
+      html: html // html body
+    });
+
     return res.status(200).json({ success: true })
   } catch (e) {
+    console.log(e)
     res.status(500).json({ error: e.message });
   }
 })
